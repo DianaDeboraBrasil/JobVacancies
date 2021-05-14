5519ff9e0b591943b374f7212aac687209e77a08
DianaDebora <diiana.info@gmail.com>
2021-05-14 20:34:37 -0300

    link da api de homologação

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   hmr: false,
-  JobVacanciesApiHost: 'https://localhost:5001/api/' // Ambiente de desenvolvimento local
+  JobVacanciesApiHost: 'https://jobvacancies-api.herokuapp.com/api/' //ambiente de homologação
+  //JobVacanciesApiHost: 'https://localhost:5001/api/' // Ambiente de desenvolvimento local
 };
 
 /*
